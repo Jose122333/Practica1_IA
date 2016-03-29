33a32cce666b159c65b7bd3065af219cc29867b7
JoseVela <JoseVela@192.168.1.223>
2016-03-29 20:21:16 +0200

    FInal

--- src/agents/Bitxo1.java
+++ src/agents/Bitxo1.java
@@ -20,12 +20,11 @@ public class Bitxo1 extends Agent {
     int vida = 0;
 
     public Bitxo1(Agents pare) {
-        super(pare, "Bitxo1", "imatges/soni.gif");
+        super(pare, "Sonic", "imatges/soni.gif");
     }
 
     @Override
     public void inicia() {
-
         setAngleVisors(10);
         setDistanciaVisors(400);
         setVelocitatLineal(4);
@@ -37,36 +36,28 @@ public class Bitxo1 extends Agent {
 
     @Override
     public void avaluaComportament() {
-        estat = estatCombat();
         vida = estat.impactesRebuts;
-        Punt puntoCercano;
+        estat = estatCombat();
+        Bonificacio bonificacionCercana;
         
-                    if(estat.temps < 10 && estat.punts > estat.puntsRival && estat.hyperespaiDisponibles > 0 && !estat.hiperEspaiActiu ){
+                    //Mirar si vamos ganando
+            if (estat.temps == 5000 && estat.punts > estat.puntsRival && estat.hyperespaiDisponibles > 0 && !estat.hiperEspaiActiu) {
                 hyperespai();
             }
-
 
         if (espera > 0) {
             espera--;
         } else {
             atura();
-            //Mirar si vamos ganando
 
 
             if (estat.enCollisio) // situació de nau bloquejada
             {
-//                colisionPermanente++;
-//                if(colisionPermanente > 10){
-//                    hyperespai();
-//                }
                 // si veu la nau, dispara
                 if (estat.objecteVisor[CENTRAL] == NAU && estat.impactesRival < 5) {
                     dispara();   //bloqueig per nau, no giris dispara
                 } else // hi ha un obstacle, gira i parteix
                 {
-//                    if(isAturat() && estat.hyperespaiDisponibles >=0){
-//                        hyperespai();
-//                    }
                     gira(20);
                     if (hiHaParedDavant(30)) {
                         enrere();
@@ -76,47 +67,48 @@ public class Bitxo1 extends Agent {
                     espera = 3;
                 }
             } else {
-                                setVelocitatLineal(4);
+
                 setVelocitatAngular(3);
-                
+
                 //Pasar modo ataque
                 if (estat.veigEnemic) {
-                    
-                    if (estat.sector == 2 || estat.sector == 3)
+
+                    if (estat.sector == 2 || estat.sector == 3) {
                         mira(estat.posicioEnemic.x, estat.posicioEnemic.y);
-                    
+                    }
+
                     if (estat.objecteVisor[CENTRAL] == NAU && estat.impactesRival < 5) {
-                    if(estat.perforadores > 0){
-                        perforadora();
-                    }else  {
-                        dispara();
-                    } 
-                }
-                    
+                        if (estat.perforadores > 0) {
+                            perforadora();
+                        } else {
+                            dispara();
+                        }
+
+                    }
+
                     if (estat.balaEnemigaDetectada && estat.impactesRebuts >= 3 && estat.hyperespaiDisponibles > 0) {
                         hyperespai();
-                    } 
-                    if (estat.balaEnemigaDetectada && estat.impactesRebuts < 3 ) activaEscut();
-                    
-                        
-                 // si no ve el enemigo 
-                } else {
-                    //Si te disparan por la espalda
-                    if(vida < estat.impactesRebuts){
-                        gira(180);
                     }
-                }
+                    if (estat.balaEnemigaDetectada && estat.impactesRebuts < 3) {
+                        activaEscut();
+                    }
 
+                    // si no ve el enemigo 
+                } else{ //Si te disparan por la espalda
+                if (vida < estat.impactesRebuts) {
+                    gira(180);
+                }
+                }
                 // Miram els visors per detectar els obstacles
                 int sensor = 0;
 
-                if (estat.objecteVisor[ESQUERRA] == PARET && estat.distanciaVisors[ESQUERRA] < 45) {
+                if (estat.objecteVisor[ESQUERRA] == PARET && estat.distanciaVisors[ESQUERRA] < 30) {
                     sensor += 1;
                 }
-                if (estat.objecteVisor[CENTRAL] == PARET && estat.distanciaVisors[CENTRAL] < 45) {
+                if (estat.objecteVisor[CENTRAL] == PARET && estat.distanciaVisors[CENTRAL] < 30) {
                     sensor += 2;
                 }
-                if (estat.objecteVisor[DRETA] == PARET && estat.distanciaVisors[DRETA] < 45) {
+                if (estat.objecteVisor[DRETA] == PARET && estat.distanciaVisors[DRETA] < 30) {
                     sensor += 4;
                 }
                 switch (sensor) {
@@ -131,7 +123,7 @@ public class Bitxo1 extends Agent {
                     case 4:
                     case 6:  // dreta bloquejada
                         setVelocitatAngular(9);
-                        esquerra();                     
+                        esquerra();
                         break;
                     case 5:
                         endavant();
@@ -144,20 +136,17 @@ public class Bitxo1 extends Agent {
                         if (distancia < 10) {
                             enrere();
                             espera = 8;
-                        } else //Girar izquierda o derecha para no perder tiempo según los grados
-                        {
-                            if (estat.angle >= 0 || estat.angle <= 180) {
+                        } else if(distancia < 50){
+                          //Girar izquierda o derecha para no perder tiempo según los grados
+                         if (estat.angle >= 0 || estat.angle <= 180) {
                                 setVelocitatAngular(9);
-                                
                                 gira(45);
                                 endavant();
-                                //esquerra();
                             } else {
-                                
-                                 setVelocitatAngular(9);
+
+                                setVelocitatAngular(9);
                                 gira(-45);
                                 endavant();
-                                //dreta();
                             }
                         }
                         break;
@@ -166,12 +155,16 @@ public class Bitxo1 extends Agent {
                     esperaBusq--;
                 } else {
                     endavant();
-                    puntoCercano = distanciaMinima();
-                    if (!hiHaParedDavant((int) estat.posicio.distancia(puntoCercano))) {
-                        mira(puntoCercano.x, puntoCercano.y);
-                    } else esperaBusq = 8;
+                    bonificacionCercana = distanciaMinima();
+                    if (!hiHaParedDavant((int) estat.posicio.distancia(bonificacionCercana.posicio))) {
+                        mira(bonificacionCercana.posicio.x, bonificacionCercana.posicio.y);
+                        if (bonificacionCercana.tipus == 5) {
+                            dispara();
+                        }
+                    } else {
+                        esperaBusq = 8;
+                    }
                 }
-                
 
             }
         }
@@ -193,13 +186,6 @@ public class Bitxo1 extends Agent {
 
         return false;
     }
-    
-    boolean hayParedEntre(int dist) {
-        if (estat.objecteVisor[CENTRAL] == PARET && estat.distanciaVisors[CENTRAL] <= dist) {
-            return true;
-        }
-                return false;
-    }
 
     double minimaDistanciaVisors() {
         double minim;
@@ -217,22 +203,19 @@ public class Bitxo1 extends Agent {
         return minim;
     }
 
-    //Devuelve el punto con la distancia mínima en bonificacions, sin contar las minas
-    Punt distanciaMinima() {
+    //Devuelve la bonificación con la distancia mínima en bonificacions, sin contar las minas
+    Bonificacio distanciaMinima() {
         int minimo;
 
         int[] distancias = new int[estat.bonificacions.length];
 
         //Copiar las distancias entre la posición actual y las bonificaciones
         for (int i = 0; i < distancias.length; i++) {
-            if (estat.bonificacions[i].tipus != 5) {
-                distancias[i] = (int) estat.posicio.distancia(estat.bonificacions[i].posicio);
-            } else {
-                distancias[i] = 1000000;
-            }
+            distancias[i] = (int) estat.posicio.distancia(estat.bonificacions[i].posicio);
+
         }
         minimo = devolverMinimo(distancias);
-        return estat.bonificacions[minimo].posicio;
+        return estat.bonificacions[minimo];
     }
 
     //Devuelve el valor mínimo del array introducido
--- src/agents/Bitxo2.java
+++ src/agents/Bitxo2.java
@@ -1,9 +1,6 @@
 package agents;
 
-// Exemple de Bitxo
-import java.util.ArrayList;
-import java.util.Arrays;
-
+// Exemple de Bitxo,
 public class Bitxo2 extends Agent {
 
     static final int PARET = 0;
@@ -16,187 +13,154 @@ public class Bitxo2 extends Agent {
 
     Estat estat;
     int espera = 0;
-    int esperaBusqueda = 0;
+    int esperaBusq = 0;
+    int vida = 0;
 
     public Bitxo2(Agents pare) {
-        super(pare, "Pikachu", "imatges/robotank2.gif");
+        super(pare, "Sonic", "imatges/soni.gif");
     }
 
     @Override
     public void inicia() {
-        setAngleVisors(30);
+        setAngleVisors(10);
         setDistanciaVisors(400);
-        setVelocitatLineal(5);
-        setVelocitatAngular(2);
+        setVelocitatLineal(4);
+        setVelocitatAngular(3);
+        estat = estatCombat();
         espera = 0;
+        esperaBusq = 0;
     }
 
     @Override
     public void avaluaComportament() {
-        boolean enemic;
-
-        enemic = false;
-
-        int dir;
-
-        activaEscut();
+        vida = estat.impactesRebuts;
         estat = estatCombat();
+        Bonificacio bonificacionCercana;
+        
+                    //Mirar si vamos ganando
+            if (estat.temps == 5000 && estat.punts > estat.puntsRival && estat.hyperespaiDisponibles > 0 && !estat.hiperEspaiActiu) {
+                hyperespai();
+            }
+
         if (espera > 0) {
             espera--;
         } else {
             atura();
 
+
             if (estat.enCollisio) // situació de nau bloquejada
             {
                 // si veu la nau, dispara
-
                 if (estat.objecteVisor[CENTRAL] == NAU && estat.impactesRival < 5) {
-                    //dispara();   //bloqueig per nau, no giris dispara
-                    perforadora();
+                    dispara();   //bloqueig per nau, no giris dispara
                 } else // hi ha un obstacle, gira i parteix
                 {
-                    gira(20); // 20 graus
-                    if (hiHaParedDavant(20)) {
+                    gira(20);
+                    if (hiHaParedDavant(30)) {
                         enrere();
                     } else {
                         endavant();
-                    }
-                    if (estat.enCollisio) {
-                        esquerra();
                     }
                     espera = 3;
                 }
             } else {
 
-                endavant();
+                setVelocitatAngular(3);
 
+                //Pasar modo ataque
                 if (estat.veigEnemic) {
+
                     if (estat.sector == 2 || estat.sector == 3) {
                         mira(estat.posicioEnemic.x, estat.posicioEnemic.y);
-                    } else if (estat.sector == 1) {
-                        dreta();
-                    } else {
-                        esquerra();
-                    }
-                }
-
-                if (estat.objecteVisor[CENTRAL] == NAU && !estat.disparant && estat.impactesRival < 5) {
-                    // dispara();
-                    if (estat.perforadores > 0) {
-                        perforadora();
-                    } else {
-                        dispara();
                     }
 
-                    activaEscut();
-                    if (estat.balaEnemigaDetectada && estat.impactesRebuts == 4) {
+                    if (estat.objecteVisor[CENTRAL] == NAU && estat.impactesRival < 5) {
+                        if (estat.perforadores > 0) {
+                            perforadora();
+                        } else {
+                            dispara();
+                        }
+
+                    }
+
+                    if (estat.balaEnemigaDetectada && estat.impactesRebuts >= 3 && estat.hyperespaiDisponibles > 0) {
                         hyperespai();
                     }
+                    if (estat.balaEnemigaDetectada && estat.impactesRebuts < 3) {
+                        activaEscut();
+                    }
+
+                    // si no ve el enemigo 
+                } else{ //Si te disparan por la espalda
+                if (vida < estat.impactesRebuts) {
+                    gira(180);
+                }
                 }
                 // Miram els visors per detectar els obstacles
                 int sensor = 0;
 
-                if (estat.objecteVisor[ESQUERRA] == PARET && estat.distanciaVisors[ESQUERRA] < 45) {
+                if (estat.objecteVisor[ESQUERRA] == PARET && estat.distanciaVisors[ESQUERRA] < 30) {
                     sensor += 1;
                 }
-                if (estat.objecteVisor[CENTRAL] == PARET && estat.distanciaVisors[CENTRAL] < 45) {
+                if (estat.objecteVisor[CENTRAL] == PARET && estat.distanciaVisors[CENTRAL] < 30) {
                     sensor += 2;
                 }
-                if (estat.objecteVisor[DRETA] == PARET && estat.distanciaVisors[DRETA] < 45) {
+                if (estat.objecteVisor[DRETA] == PARET && estat.distanciaVisors[DRETA] < 30) {
                     sensor += 4;
                 }
-                setVelocitatLineal(3);
                 switch (sensor) {
                     case 0:
-                        setVelocitatLineal(5);
-                        setVelocitatAngular(2);
                         endavant();
-
-                        if (esperaBusqueda > 0) {
-                            esperaBusqueda--;
-                        } else {
-                            Bonificacio p;
-                            p = distanciabonificacions();
-                            if (estat.bonificacions.length < 2) {
-                                mira(estat.posicioEnemic.x, estat.posicioEnemic.y);
-
-                            } else if (p.tipus == MINA) {
-                                mira(p.posicio.x, p.posicio.y);
-                                dispara();
-                                p = distanciabonificacions();
-                                mira(p.posicio.x, p.posicio.y);
-
-                            } else if (!hiHaParedDavant(1)) {
-                                if (calcDistancia(p.posicio.x, p.posicio.y) < 150) {
-                                    mira(p.posicio.x, p.posicio.y);
-                                }
-                            }
-                        }
-
                         break;
-                    case 1:  // esquerra bloquejada
-                        espera = 5;
-                        esperaBusqueda = 15;
-                        setVelocitatAngular(5);
+                    case 1:
+                    case 3:  // esquerra bloquejada
+                        setVelocitatAngular(9);
                         dreta();
-
                         break;
-                    case 3:  // esquerra  i centro bloquejada
-                        esperaBusqueda = 15;
-                        espera = 5;
-                        setVelocitatAngular(5);
-                        dreta();
-
-                        break;
-                    case 4: // dreta bloquejada
-                        esperaBusqueda = 15;
-                        espera = 5;
-                        setVelocitatAngular(5);
+                    case 4:
+                    case 6:  // dreta bloquejada
+                        setVelocitatAngular(9);
                         esquerra();
-
                         break;
-                    case 6:  // dreta i centro  bloquejada
-                        esperaBusqueda = 15;
-                        espera = 5;
-                        setVelocitatAngular(5);
-                        esquerra();
-
-                        break;
-
-                    case 5: //esquerra i dreta bloquetjat
-                        esperaBusqueda = 15;
-                        setVelocitatLineal(5);
-                        setVelocitatAngular(2);
+                    case 5:
                         endavant();
                         break;  // centre lliure
                     case 2:  // paret devant
-                        esperaBusqueda = 15;
-                        espera = 5;
-                        setVelocitatAngular(5);
-                        esquerra();
-                        if (estat.enCollisio) {
-                            dreta();
-                        }
-
-                    case 7:  // si estic molt aprop, --> torna enrere esuqerra dreta i centro bloquetjat
+                    case 7:  // si estic molt aprop, torna enrere
                         double distancia;
                         distancia = minimaDistanciaVisors();
 
-                        if (distancia < 3) {
+                        if (distancia < 10) {
+                            enrere();
                             espera = 8;
-                            // enrere();
-                            gira(180);
-                        } else // gira aleatòriament a la dreta o a l'esquerra
-                        //                        if (distancia < 50) {
-                        //                            if (Math.random() * 500 < 250) {
-                        //                                dreta();
-                        //                            } else {
-                        {
-                            esquerra();
+                        } else if(distancia < 50){
+                          //Girar izquierda o derecha para no perder tiempo según los grados
+                         if (estat.angle >= 0 || estat.angle <= 180) {
+                                setVelocitatAngular(9);
+                                gira(45);
+                                endavant();
+                            } else {
+
+                                setVelocitatAngular(9);
+                                gira(-45);
+                                endavant();
+                            }
                         }
-//                            }
-//                        }
                         break;
+                }
+                if ((esperaBusq > 0)) {
+                    esperaBusq--;
+                } else {
+                    endavant();
+                    bonificacionCercana = distanciaMinima();
+                    if (!hiHaParedDavant((int) estat.posicio.distancia(bonificacionCercana.posicio))) {
+                        mira(bonificacionCercana.posicio.x, bonificacionCercana.posicio.y);
+                        if (bonificacionCercana.tipus == 5) {
+                            dispara();
+                        }
+                    } else {
+                        esperaBusq = 8;
+                    }
                 }
 
             }
@@ -236,48 +200,34 @@ public class Bitxo2 extends Agent {
         return minim;
     }
 
-    Bonificacio distanciabonificacions() {
-        int[] distancia = new int[estat.bonificacions.length];
-        // Punt p = new Punt();
-        int dx;
-        int dy;
-        int dist;
-        int mx = estat.posicio.x;
-        int my = estat.posicio.y;
-        int menor;
-        int distan;
+    //Devuelve la bonificación con la distancia mínima en bonificacions, sin contar las minas
+    Bonificacio distanciaMinima() {
+        int minimo;
 
-        for (int i = 0; i < estat.bonificacions.length; i++) {
+        int[] distancias = new int[estat.bonificacions.length];
 
-            distancia[i] = calcDistancia(estat.bonificacions[i].posicio.x, estat.bonificacions[i].posicio.y);
+        //Copiar las distancias entre la posición actual y las bonificaciones
+        for (int i = 0; i < distancias.length; i++) {
+            distancias[i] = (int) estat.posicio.distancia(estat.bonificacions[i].posicio);
 
         }
+        minimo = devolverMinimo(distancias);
+        return estat.bonificacions[minimo];
+    }
 
-        menor = 0;
-        distan = distancia[0];
-        for (int i = 1; i < distancia.length; i++) {
-            if (distancia[i] <= distan) {
-                menor = i;
-                distan = distancia[i];
+    //Devuelve el valor mínimo del array introducido
+    int devolverMinimo(int[] distancias) {
+        int minimo;
+        int indiceMin = 0;
+        if (distancias.length > 0) {
+            minimo = distancias[0];
+            for (int i = 0; i < distancias.length; i++) {
+                if (distancias[i] < minimo) {
+                    minimo = distancias[i];
+                    indiceMin = i;
+                }
             }
         }
-        //p.x = estat.bonificacions[menor].posicio.x;
-        //p.y = estat.bonificacions[menor].posicio.y;
-
-        return estat.bonificacions[menor];
+        return indiceMin;
     }
-
-    int calcDistancia(int x, int y
-    ) {
-        int mx = estat.posicio.x;
-        int my = estat.posicio.y;
-        int dx;
-        int dy;
-        dx = x - mx; //punto final menos el inicial 
-        dy = y - my;
-        double re = (double) dx * dx + dy * dy;
-        int i = (int) Math.sqrt(re);
-        return i;
-    }
-
 }
